5eefd1132190920f0a243474a63306c89e3ca68a
enejsimrajh <59830146+enejsimrajh@users.noreply.github.com>
2023-07-06 22:23:21 +0000

    Configuration

--- crates/weather-client/src/main.rs
+++ crates/weather-client/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    extract::{Host, WebSocketUpgrade},
+    response::Html,
+    routing::get,
+    Router,
+};
+use dioxus::prelude::*;
+use tower_http::services::ServeDir;
+
+fn app(cx: Scope) -> Element {
+    let mut num = use_state(cx, || 0);
+
+    cx.render(rsx! {
+        div {
+            "hello axum! {num}"
+            button { onclick: move |_| num += 1, "Increment" }
+        }
+    })
 }
