a2a98305853dddaab93eaefd7cd7cf7859b2e6d0
enejsimrajh <59830146+enejsimrajh@users.noreply.github.com>
2023-07-06 13:35:54 +0000

    Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    ".crates/weather-client/src/**/*.{html,rs}",
+    "./crates/weather-client/src/**/*.{html,rs}",
   ],
   theme: {
     extend: {},
